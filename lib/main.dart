47aaf93cbf4769ef54c6277e818149a713a024f0
enchance <enchance@gmail.com>
2022-09-25 18:16:07 +0800

    use routes instead of onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
         ),
-        home: CalculatorScreen(),
-        onGenerateRoute: getRoutes(),
+        routes: getRoutes(context),
       ),
     );
   }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:simplecalc/screens/calculator_screen.dart';
-import 'package:simplecalc/screens/settings_screen.dart';
+
+import './screens/calculator_screen.dart';
+import './screens/settings_screen.dart';
 
 
 
-RouteFactory getRoutes() {
-  return (settings) {
-    final Object? args = settings.arguments;
-    Widget screen;
-
-    switch(settings.name) {
-      case SettingsScreen.route:
-        screen = const SettingsScreen();
-        break;
-      default:
-        screen = CalculatorScreen();
-    }
-    
-    return MaterialPageRoute(builder: (BuildContext context) => screen);
+Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
+  return {
+    '/': (context) => CalculatorScreen(),
+    SettingsScreen.route: (context) => SettingsScreen(),
   };
 }
